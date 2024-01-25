46bd484b41377a9c4a01367a4b5801412aff0092
Ben Overmyer <ben@overmyer.net>
2024-01-25 05:23:12 -0500

    Add docs

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,35 @@
-'use strict';
+import random from "random";
 
-import random from 'random';
-
+/**
+ * This function takes a maximum value and returns a random number between 1
+ * and that value.
+ *
+ * @param {Number} max - The maximum value to return
+ * @returns {Number} - A random number between 1 and the maximum value
+ */
 export function simple(max: number): number {
-  /**
-   * This function takes a maximum value and returns a random number between 1
-   * and that value.
-   * 
-   * @param {Number} max - The maximum value to return
-   * @returns {Number} - A random number between 1 and the maximum value
-   */
   return random.int(1, max);
 }
 
+/**
+ * This function takes an array and returns a random item from it.
+ *
+ * @param {Array} items - The array to get the item from
+ * @returns {Any} - A random item from the array
+ */
 export function item(items: any[]) {
-  /**
-   * This function takes an array and returns a random item from it.
-   * 
-   * @param {Array} items - The array to get the item from
-   * @returns {Any} - A random item from the array
-   */
   return items[random.int(0, items.length - 1)];
 }
 
+/**
+ * This function takes a minimum and maximum value and returns a random float
+ * between them, weighted towards the middle.
+ *
+ * @param {Number} min - The minimum value to return
+ * @param {Number} max - The maximum value to return
+ * @returns {Number} - A random float between the minimum and maximum values
+ */
 export function bellFloat(min: number, max: number): number {
-  /**
-   * This function takes a minimum and maximum value and returns a random float
-   * between them, weighted towards the middle.
-   * 
-   * @param {Number} min - The minimum value to return
-   * @param {Number} max - The maximum value to return
-   * @returns {Number} - A random float between the minimum and maximum values
-   */
   const divisor = (max - min) / 3;
 
   let result = min;
@@ -43,15 +41,15 @@ export function bellFloat(min: number, max: number): number {
   return result;
 }
 
+/**
+ * This function takes an array and returns a random set of items from it.
+ *
+ * @param {Number} itemCount - The number of items to return
+ * @param {Array} items - The array to get the items from
+ * @returns {Array} - A random set of items from the array
+ */
 export function randomSet(itemCount: number, items: any[]): any[] {
-  /**
-   * This function takes an array and returns a random set of items from it.
-   * 
-   * @param {Number} itemCount - The number of items to return
-   * @param {Array} items - The array to get the items from
-   * @returns {Array} - A random set of items from the array
-   */
-  let result: any = [];
+  const result: any = [];
 
   items = shuffle(items);
 
@@ -62,14 +60,14 @@ export function randomSet(itemCount: number, items: any[]): any[] {
   return result;
 }
 
+/**
+ * This function takes a length and returns a random string of that length.
+ *
+ * @param {Number} length - The length of the string to return
+ * @returns {String} - A random string of the specified length
+ */
 export function randomString(length: number): string {
-  /**
-   * This function takes a length and returns a random string of that length.
-   * 
-   * @param {Number} length - The length of the string to return
-   * @returns {String} - A random string of the specified length
-   */
-  let result: string = '';
+  let result = "";
 
   for (let i = 0; i < length; i++) {
     result += Math.random().toString(36).slice(2)[0];
@@ -78,13 +76,13 @@ export function randomString(length: number): string {
   return result;
 }
 
+/**
+ * This function takes an array and returns a shuffled version of it.
+ *
+ * @param {Array} items - An array of items to shuffle
+ * @returns {Array} - A shuffled version of the array
+ */
 export function shuffle(items: any[]) {
-  /**
-   * This function takes an array and returns a shuffled version of it.
-   * 
-   * @param {Array} items - An array of items to shuffle
-   * @returns {Array} - A shuffled version of the array
-   */
   for (let i = items.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i);
     const temp = items[i];
@@ -95,23 +93,23 @@ export function shuffle(items: any[]) {
   return items;
 }
 
+/**
+ * This function takes an array of objects with a commonality property
+ * and returns a random result, weighted by the commonality property.
+ *
+ * @param {Array} items - An array of objects with a commonality property
+ * @returns {Object} - A random object from the array, weighted by the commonality property
+ */
 export function weighted(items: any[]) {
-  /**
-   * This function takes an array of objects with a commonality property
-   * and returns a random result, weighted by the commonality property.
-   * 
-   * @param {Array} items - An array of objects with a commonality property
-   * @returns {Object} - A random object from the array, weighted by the commonality property
-   */
   let ceiling = 0;
 
-  if (items.length == 1) {
+  if (items.length === 1) {
     return items[0];
   }
 
-  items.forEach(function (item: any) {
+  for (const item of items) {
     ceiling += item.commonality;
-  });
+  }
 
   let randomValue = random.int(0, ceiling);
 
